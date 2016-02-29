9c90ca5d26d1c67a4fee61df1da867e4470282f4
hua3505 <959547664@qq.com>
2016-02-29 14:22:52 +0800

    add button whick is to start wechat's payment activity when clicked

--- app/src/main/java/com/gmail/huashadow/quickpay/MainActivity.java
+++ app/src/main/java/com/gmail/huashadow/quickpay/MainActivity.java
@@ -3,6 +3,11 @@ package com.gmail.huashadow.quickpay;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
 
 /**
  * Created by Wolf Xu on 2016/2/26.
@@ -16,5 +21,38 @@ public class MainActivity extends Activity {
         Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        setWechatBtnClickAction();
+    }
+
+    private void setWechatBtnClickAction() {
+        Button btnWechat = (Button) findViewById(R.id.btn_pay_by_wechat);
+        btnWechat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityWithRoot("com.tencent.mm/.plugin.offline.ui.WalletOfflineCoinPurseUI");
+            }
+        });
+    }
+
+    /**
+     *
+     * @param activity com.package.name/com.package.name.ActivityName
+     */
+    private void startActivityWithRoot(String activity) {
+        Process process = null;
+        try {
+            process = Runtime.getRuntime().exec("su");
+            DataOutputStream outputStream = new DataOutputStream(process.getOutputStream());
+            String command = "am start -n " + activity +  "\n";
+            outputStream.writeBytes(command);
+            outputStream.writeBytes("exit\n");
+            outputStream.flush();
+        } catch (IOException e) {
+            Log.e(TAG, "startActivityWithRoot", e);
+        }
     }
 }
